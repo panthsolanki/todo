5256817e48ee8a35bba30ded4e94e6484f609728
Panth <panth.solanki@gmail.com>
2019-10-18 17:32:43 +0530

    pages created

---
+++ src/pages/nopage.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class noPage extends Component {
+  render() { 
+    return ( 
+      <h1>No Page Available</h1>
+    );
+  }
+}
+ 
+export default noPage;
