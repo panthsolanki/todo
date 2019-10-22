6a1d5dbc111135a6f3f8088b1cd8f033d6f9229e
Panth <panth.solanki@gmail.com>
2019-10-22 16:50:04 +0530

    list connected with firestore

--- src/pages/list.js
+++ src/pages/list.js
@@ -1,12 +1,36 @@
 import React, { Component } from 'react';
+import  { FirebaseContext, withFirebase } from '../Firebase';
 
 class List extends Component {
-  state = {  }
+  state = { 
+    todoList: [],
+   }
+  componentDidMount() {
+    this.props.firebase.db.collection("todos")
+    .get()
+    .then(querySnapshot => {
+      const todoList = [];
+      querySnapshot.docs.forEach(doc => {
+        const todo = {};
+        todo.id = doc.id;
+        Object.assign(todo, doc.data());
+        todoList.push(todo);
+      });
+      this.setState({todoList})
+      console.log('this.state.todoList: ', this.state.todoList);
+    });
+  }
   render() { 
-    return ( 
-      <h1>List</h1>
+    return (
+      <FirebaseContext.Consumer>        
+        {firebase => {
+          return (<div>
+            <h1>List with firestore</h1>
+          </div>);
+        }}
+      </FirebaseContext.Consumer>
     );
   }
 }
  
-export default List;
+export default withFirebase(List);
