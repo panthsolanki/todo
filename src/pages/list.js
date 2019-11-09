ccc7e2a0ac9af88c1d0c9d3db648ed26e5ccb412
Panth <panth.solanki@gmail.com>
2019-11-09 22:47:44 +0530

    list page updated

--- src/pages/list.js
+++ src/pages/list.js
@@ -1,11 +1,9 @@
 import React, { Component } from 'react';
 import  { FirebaseContext, withFirebase } from '../Firebase';
+import { connect } from 'react-redux';
 
 class List extends Component {
-  state = { 
-    todoList: [],
-   }
-  componentDidMount() {
+  render() { 
     this.props.firebase.db.collection("todos")
     .get()
     .then(querySnapshot => {
@@ -16,16 +14,20 @@ class List extends Component {
         Object.assign(todo, doc.data());
         todoList.push(todo);
       });
-      this.setState({todoList})
-      console.log('this.state.todoList: ', this.state.todoList);
+      this.props.getList(todoList);
     });
-  }
-  render() { 
     return (
       <FirebaseContext.Consumer>        
         {firebase => {
           return (<div>
-            <h1>List with firestore</h1>
+            <h1>List</h1>
+            <ul>
+              {this.props.todoList.map((todo) => (
+                <li key={todo.id}>
+                  {todo.task} {todo.date} {todo.time} {todo.status}
+                </li>
+              ))}
+            </ul>
           </div>);
         }}
       </FirebaseContext.Consumer>
@@ -33,4 +35,16 @@ class List extends Component {
   }
 }
  
-export default withFirebase(List);
+const mapStateToProps = (state) => {
+  return {
+    todoList: state.todoList,
+  }
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    getList: (todoList) => dispatch({type:'GET_LIST', value:todoList}),
+  }
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(withFirebase(List));
